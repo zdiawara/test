7768332ccf3c11cff7853eb3fe5de355323b4b73
Zakaridia DIAWARA <zakaridia.diawara@maif.fr>
2022-10-30 22:45:34 +0100

    test

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,45 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { useState } from "react";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [result, setResultat] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const seConnecter = () => {
+    setResultat(null);
+    setLoading(true);
+    fetch("https://rci.burvalgroup.com/backend/public/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: username,
+        password,
+      }),
+    })
+      .then((r) => r.json())
+      .then((e) => {
+        setResultat(JSON.stringify(e));
+      })
+      .catch((e) => {
+        setResultat(JSON.stringify(e));
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +53,24 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonInput
+          name="username"
+          placeholder="Identifiant"
+          onIonChange={(e) => {
+            setUsername((e.target.value || "") as string);
+          }}
+        />
+        <IonInput
+          name="password"
+          placeholder="Mot de passe"
+          onIonChange={(e) => {
+            setPassword((e.target.value || "") as string);
+          }}
+        />
+        <IonButton onClick={seConnecter}>
+          {loading ? "En cours ..." : "Se connecter"}
+        </IonButton>
+        <div>{result}</div>
       </IonContent>
     </IonPage>
   );
